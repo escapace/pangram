136bf0dd543b8b128e85f04984f2025bba09c6b3
yyxi <178086254+yyxi@users.noreply.github.com>
2024-12-02 21:38:00 +0000

    fix: build attempt

--- packages/container/src/index.ts
+++ packages/container/src/index.ts
@@ -1,4 +1,5 @@
 export { fontMetrics } from './font/font-metrics'
 export type { FontMetrics } from './font/font-metrics'
 export { TypeFontIssue } from './font/font-metrics'
+
 export { schemaFallback } from './state/user-schema'
